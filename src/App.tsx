a8d97d96ba191e71f8c299e0a0386e0d4e22ea47
Eduardo Moura <emoufilho@gmail.com>
2026-03-01 18:39:37 -0300

    feat: inclusao admob

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { View } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
@@ -7,6 +7,7 @@ import { AppRoutes } from './AppRoutes';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { Theme } from './shared/themes/Theme';
 import { StatusBar } from 'expo-status-bar';
+import MobileAds, { BannerAd, BannerAdSize, TestIds, AdsConsent } from 'react-native-google-mobile-ads';
 
 
 SplashScreen.preventAutoHideAsync();
@@ -24,17 +25,37 @@ export function App() {
     }
   }, [loaded, error]);
 
+  useEffect(() => {
+    const init = async () => {
+      try {
+        await AdsConsent.gatherConsent();
+      } catch (error) {
+        console.log(error);
+      }
+      MobileAds().initialize();
+    }
+    init();
+  }, []);
+
+  const adsUnitId = useMemo(() => {
+    if (__DEV__) return TestIds.BANNER;
+    return 'ca-app-pub-4253572183166318/2172045062';
+  }, []);
+
   if (!loaded && !error) {
     return null;
   }
-
 
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1, backgroundColor: Theme.colors.background }}>
         <StatusBar style="light" />
         <AppRoutes />
-      </SafeAreaView>
+        <BannerAd 
+          unitId={adsUnitId}
+          size={BannerAdSize.FULL_BANNER}
+        />  
+      </SafeAreaView> 
     </SafeAreaProvider>
     
   );
